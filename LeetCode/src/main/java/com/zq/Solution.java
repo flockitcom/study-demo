587dfa59cece6f6b3ec41f7b88a94fd785a80671
flockitcom <13294141428@163.com>
2022-08-30 19:07:08 +0800

    fix-KMP

--- LeetCode/src/main/java/com/zq/Solution.java
+++ LeetCode/src/main/java/com/zq/Solution.java
@@ -1,7 +1,32 @@
 package com.zq;
 
 class Solution {
-    public int strStr(String haystack, String needle) {
-        return 0;
+    public boolean repeatedSubstringPattern(String s) {
+        int len = s.length();
+        if (len == 0) {
+            return false;
+        }
+        int[] next = new int[len];
+        int i = 0;
+        next[0] = 0;
+        for (int j = 1; j < len; j++) {
+            while (i > 0 && s.charAt(i) != s.charAt(j)) {
+                i = next[i - 1];
+            }
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+            }
+            next[j] = i;
+        }
+        if (next[len - 1] > 0 && len % (len - next[len - 1]) == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        boolean i = solution.repeatedSubstringPattern("aabaabaab");
+        System.out.println(i);
     }
 }
