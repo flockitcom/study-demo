6d840dd2f46143c84f342c052baa212d645e2568
flockitcom <13294141428@163.com>
2022-09-23 22:31:24 +0800

    fix-最小体力爬楼梯

--- LeetCode/src/main/java/com/zq/Solution.java
+++ LeetCode/src/main/java/com/zq/Solution.java
@@ -3,7 +3,11 @@ package com.zq;
 class Solution {
 
     public int minCostClimbingStairs(int[] cost) {
-        return 0;
+        int[] dp = new int[cost.length];
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+        }
+        return Math.min(dp[cost.length - 2] + cost[cost.length - 2], dp[cost.length - 1] + cost[cost.length - 1]);
     }
 
     public static void main(String[] args) {
