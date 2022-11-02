3532c0fe4043c86c3a28dfc54959069b5c86f530
flockitcom <13294141428@163.com>
2022-11-02 19:08:58 +0800

    fix-多模块 web启动

--- LeetCode/src/main/java/com/zq/Solution.java
+++ LeetCode/src/main/java/com/zq/Solution.java
@@ -1,83 +1,34 @@
 package com.zq;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
 
-    List<List<String>> result = new ArrayList<>();
-    List<String> list = new ArrayList<>();
-    char[] chars;
-    boolean[][] use;
-
-    public List<List<String>> solveNQueens(int n) {
-        use = new boolean[n][n];
-        chars = new char[n];
-        Arrays.fill(chars, '.');
-        backTracking(n, 0);
-        return result;
+        //当前差值
+        int curDiff = 0;
+        //上一个差值
+        int preDiff = 0;
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            //得到当前差值
+            curDiff = nums[i] - nums[i - 1];
+            //如果当前差值和上一个差值为一正一负
+            //等于0的情况表示初始时的preDiff
+            if ((curDiff > 0 && preDiff <= 0) || (curDiff < 0 && preDiff >= 0)) {
+                count++;
+                preDiff = curDiff;
+            }
+        }
+        return count;
     }
 
-    public void backTracking(int n, int startIndex) {
-        if (startIndex >= n) {
-            result.add(new ArrayList<>(list));
-        }
-        for (int i = 0; i < n; i++) {
-            boolean flag = false;
-
-            //判断列是否存在
-            if (!flag) {
-                for (int j = 0; j < startIndex; j++) {
-                    if (use[j][i]) {
-                        flag = true;
-                        break;
-                    }
-                }
-            }
-
-            //判断左上是否存在
-            if (!flag) {
-                int leftMin = Math.min(startIndex, i);
-                for (int l = 0; l < leftMin; l++) {
-                    if (use[startIndex - l - 1][i - l - 1]) {
-                        flag = true;
-                        break;
-                    }
-                }
-            }
-            if (!flag) {
-                //判断右上是否存在
-                int rightMin = Math.min(startIndex, n - 1 - i);
-                for (int r = 0; r < rightMin; r++) {
-                    if (use[startIndex - r - 1][i + r + 1]) {
-                        flag = true;
-                        break;
-                    }
-                }
-            }
-
-            if (flag) {
-                continue;
-            }
-
-
-            chars[i] = 'Q';
-            use[startIndex][i] = true;
-            list.add(new String(chars));
-            chars[i] = '.';
-            backTracking(n, startIndex + 1);
-            list.remove(list.size() - 1);
-            use[startIndex][i] = false;
-
-        }
-    }
-
-
+    //g = [1,2,3], s = [1,1]
     public static void main(String[] args) {
         Solution solution = new Solution();
-        Object o = solution.solveNQueens(4);
-        System.out.println("" + o);
+        Object o = solution.wiggleMaxLength(new int[]{1, 7, 4, 9, 2, 5});
+        System.out.println(o);
     }
 }
 
