a79402999f7efbf7fdb3f8f643e70356cb11a17f
flockitcom <13294141428@163.com>
2022-11-25 18:58:13 +0800

    fix-删除二叉搜索树中的节点

--- LeetCode/src/main/java/com/zq/Solution.java
+++ LeetCode/src/main/java/com/zq/Solution.java
@@ -1,38 +1,46 @@
 package com.zq;
 
 class Solution {
-    int abs = Integer.MAX_VALUE;
 
-    public int getMinimumDifference(TreeNode root) {
+    TreeNode pre = null;
+    TreeNode deleteNode;
 
-        return 1;
+    public TreeNode deleteNode(TreeNode root, int key) {
+        tree(root, key);
+        return root;
+    }
+
+    private void tree(TreeNode root, int val) {
+
+        if (val < root.val) {
+        }
     }
 
 
     public static void main(String[] args) {
 
-        TreeNode node1 = new TreeNode(236);
-        TreeNode node2 = new TreeNode(104);
-        TreeNode node3 = new TreeNode(701);
-        TreeNode node4 = new TreeNode(227);
-        TreeNode node5 = new TreeNode(911);
-        TreeNode node6 = new TreeNode(4);
-        TreeNode node7 = new TreeNode(6);
+        TreeNode node1 = new TreeNode(5);
+        TreeNode node2 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(6);
+        TreeNode node4 = new TreeNode(2);
+        TreeNode node5 = new TreeNode(4);
+        TreeNode node6 = new TreeNode(7);
+        TreeNode node7 = new TreeNode(9);
         TreeNode node8 = new TreeNode(3);
         TreeNode node9 = new TreeNode(5);
         TreeNode node10 = new TreeNode(1);
         node1.left = node2;
         node1.right = node3;
-        node2.right = node4;
-        node3.right = node5;
-//        node3.left = node6;
+        node2.left = node4;
+        node2.right = node5;
+        node3.right = node6;
 //        node3.right = node7;
-//        node5.right = node8;
-//        node6.left = node9;
+//        node5.left = node8;
+//        node5.right = node9;
 //        node6.right = node10;
 
         Solution solution = new Solution();
-        Object o = solution.getMinimumDifference(node1);
+        Object o = solution.deleteNode(node1, 3);
         System.out.println(o);
     }
 }
