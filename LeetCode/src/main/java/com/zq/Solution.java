90b4c2d54b52557daa3f7a5ed00efd1257ced4da
flockitcom <13294141428@163.com>
2022-12-02 18:40:29 +0800

    fix-整数拆分

--- LeetCode/src/main/java/com/zq/Solution.java
+++ LeetCode/src/main/java/com/zq/Solution.java
@@ -1,50 +1,68 @@
 package com.zq;
 
 class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
-        return tree(nums, 0, nums.length);
+
+    int[][] dp;
+    int result = 1;
+
+    public int integerBreak(int n) {
+        dp = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                dp[i] = new int[]{0, 1};
+            } else {
+                if (i % 2 != 0) {
+                    dp[i][0] = dp[i - 1][0] + 1;
+                    dp[i][1] = dp[i - 1][1];
+                } else {
+                    dp[i][0] = dp[i - 1][0];
+                    dp[i][1] = dp[i - 1][1] + 1;
+                }
+            }
+        }
+        if (n > 3) {
+            maxTake(n);
+        } else {
+            return dp[n - 1][0] * dp[n - 1][1];
+        }
+        return result;
     }
 
-    private TreeNode tree(int[] nums, int start, int stop) {
-        if (start >= stop) {
-            return null;
+    private void maxTake(int nums) {
+        if (nums <= 3) {
+            result = result * nums;
+            return;
         }
-        int middle = (start + stop - 1) / 2;
-
-        TreeNode node = new TreeNode();
-        node.val = nums[middle];
-
-        node.left = tree(nums, start, middle);
-        node.right = tree(nums, middle + 1, stop);
-
-        return node;
+        maxTake(dp[nums - 1][0]);
+        maxTake(dp[nums - 1][1]);
     }
 
 
     public static void main(String[] args) {
 
-        TreeNode node1 = new TreeNode(3);
-        TreeNode node2 = new TreeNode(0);
-        TreeNode node3 = new TreeNode(4);
-        TreeNode node4 = new TreeNode(2);
-        TreeNode node5 = new TreeNode(1);
-        TreeNode node6 = new TreeNode(8);
+        TreeNode node1 = new TreeNode(30);
+        TreeNode node2 = new TreeNode(36);
+        TreeNode node3 = new TreeNode(21);
+        TreeNode node4 = new TreeNode(36);
+        TreeNode node5 = new TreeNode(35);
+        TreeNode node6 = new TreeNode(26);
         TreeNode node7 = new TreeNode(15);
-        TreeNode node8 = new TreeNode(7);
-        TreeNode node9 = new TreeNode(5);
+        TreeNode node8 = new TreeNode(33);
+        TreeNode node9 = new TreeNode(8);
         TreeNode node10 = new TreeNode(1);
         node1.left = node2;
         node1.right = node3;
-        node2.right = node4;
-        node4.left = node5;
-//        node5.left = node6;
-//        node5.right = node7;
-//        node6.left = node8;
-//        node5.right = node9;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+        node5.right = node8;
+        node7.right = node9;
 //        node6.right = node10;
 
         Solution solution = new Solution();
-        Object o = solution.sortedArrayToBST(new int[]{-10, -3, 0, 5, 9});
+        Object o = solution.integerBreak(8);
         System.out.println(o);
     }
 }
--- LeetCode/src/main/java/com/zq/TreeNode.java
+++ LeetCode/src/main/java/com/zq/TreeNode.java
@@ -22,18 +22,24 @@ public class TreeNode {
     }
 
     public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(2);
-        TreeNode node4 = new TreeNode(3);
-        TreeNode node5 = new TreeNode(4);
-        TreeNode node6 = new TreeNode(4);
-        TreeNode node7 = new TreeNode(3);
+        TreeNode node1 = new TreeNode(30);
+        TreeNode node2 = new TreeNode(36);
+        TreeNode node3 = new TreeNode(21);
+        TreeNode node4 = new TreeNode(36);
+        TreeNode node5 = new TreeNode(35);
+        TreeNode node6 = new TreeNode(26);
+        TreeNode node7 = new TreeNode(15);
+        TreeNode node8 = new TreeNode(33);
+        TreeNode node9 = new TreeNode(8);
+        TreeNode node10 = new TreeNode(1);
         node1.left = node2;
         node1.right = node3;
         node2.left = node4;
         node2.right = node5;
         node3.left = node6;
         node3.right = node7;
+        node5.right = node8;
+        node7.right = node9;
+//        node6.right = node10;
     }
 }
