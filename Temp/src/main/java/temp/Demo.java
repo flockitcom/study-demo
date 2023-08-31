9017d4c8a5f75b8f4569d6855c786e7ff41f7f41
flockitcom <13294141428@163.com>
2023-08-31 16:12:19 +0800

    fix-测试

--- Temp/src/main/java/temp/Demo.java
+++ Temp/src/main/java/temp/Demo.java
@@ -1,16 +1,11 @@
 package temp;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 /**
  * @author zqian
  * @date 2021/5/12
  */
 public class Demo {
     public static void main(String[] args) {
-       
+        System.out.println("bbb");
     }
 }
