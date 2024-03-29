850e46fac4dd788cfa8a588f77c292b79a2dbabd
flockitcom <13294141428@163.com>
2024-03-29 17:35:46 +0800

    fix-protocol buffers

---
+++ Util/src/main/java/util/ProtoBufUtil.java
@@ -0,0 +1,27 @@
+package util;
+
+/**
+ * protocol buffers
+ *
+ * @author zhangqian
+ */
+public class ProtoBufUtil {
+    public static void main(String[] args) {
+        ResponseOuterClass.Response.Builder builder = ResponseOuterClass.Response.newBuilder();
+        // 设置字段值
+        builder.setData("hello www.tizi365.com");
+        builder.setStatus(200);
+
+        ResponseOuterClass.Response response = builder.build();
+        // 将数据根据protobuf格式，转化为字节数组
+        byte[] byteArray = response.toByteArray();
+
+        // 反序列化,二进制数据
+        try {
+            ResponseOuterClass.Response newResponse = ResponseOuterClass.Response.parseFrom(byteArray);
+            System.out.println(newResponse.getData());
+            System.out.println(newResponse.getStatus());
+        } catch (Exception e) {
+        }
+    }
+}
